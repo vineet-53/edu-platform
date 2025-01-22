75816706c9ab2b1bb501c45851222dcd829f88bd
vineet-53 <vineetsinghnegi53@gmail.com>
2025-01-22 09:55:22 +0530

    init frontend

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,58 @@
+import Logo1 from "./TimeLineLogo/Logo1.svg"
+import Logo2 from "./TimeLineLogo/Logo2.svg"
+import Logo3 from "./TimeLineLogo/Logo3.svg"
+import Logo4 from "../assets/TimeLineLogo/Logo4.svg"
+import LogoFullDark from "./Logo/Logo-Full-Dark.png"
+import LogoFullLight from "./Logo/Logo-Full-Light.png"
+import LogoSmallDark from "./Logo/Logo-Small-Dark.png"
+import LogoSmallLight from "./Logo/Logo-Small-Light.png"
+import Aboutus1 from "./Images/aboutus1.webp" 
+import Aboutus2 from "./Images/aboutus2.webp" 
+import Aboutus3 from "./Images/aboutus3.webp" 
+import Banner from "./Images/banner.mp4" 
+import BgHome  from "./Images/bghome.svg" 
+import BoxOffice from "./Images/boxoffice.png" 
+import CompareWithOthers from "./Images/Compare_with_others.png" 
+import CompareWithOthersSvg  from "./Images/Compare_with_others.svg" 
+import Frame from "./Images/frame.png" 
+import FoundingStory from "./Images/FoundingStory.png" 
+import Instructor from "./Images/Instructor.png" 
+import KnowYourProgress from "./Images/Know_your_progress.png" 
+import KnowYourProgressSvg from "./Images/Know_your_progress.svg" 
+import Login from "./Images/login.webp" 
+import Signup from "./Images/signup.webp" 
+import PlanYourLessons from "./Images/Plan_your_lessons.png" 
+import PlanYourLessonsSvg from "./Images/Plan_your_lessons.svg" 
+import TimeLineLogo from "./Images/TimelineImage.png"
+
+export { 
+    Logo1,
+    Logo2,
+    Logo3,
+    Logo4,
+    LogoFullDark,
+    LogoFullLight,
+    LogoSmallDark,
+    LogoSmallLight,
+    Aboutus1,
+    Aboutus2,
+    Aboutus3,
+    Banner,
+    BgHome,
+    BoxOffice,
+    CompareWithOthers,
+    CompareWithOthersSvg,
+    Frame,
+    FoundingStory,
+    Instructor,
+    KnowYourProgress,
+    KnowYourProgressSvg,
+    Login,
+    PlanYourLessons,
+    PlanYourLessonsSvg,
+    Signup,
+    TimeLineLogo,
+}
+
+
+
